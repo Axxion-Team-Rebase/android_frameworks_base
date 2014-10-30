583d3f830fde183179aab8737b587dc3228629ac
cristianomatos <cristianobmatos@gmail.com>
2014-10-30 19:02:56 -0200

    Avoid SysUI force closing when enable/disable DSB System bar gradient option
    Change-Id: I5d6dcea9f4d7187a9c14d8ef0dd5c1c874cb8d9f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -180,7 +180,10 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
 
         public void setOverrideGradientAlpha(final int alpha) {
             mOverrideGradientAlpha = alpha;
-            generateAnimator().start();
+            try {
+                generateAnimator().start();
+            } catch (Exception e) {
+            }
         }
     }
 
