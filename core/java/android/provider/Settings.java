6b5ab0068b82563fdde4c76840d550c0a6cda912
XXMrHyde <xxmrhyde@gmx.co.uk>
2014-12-24 02:56:43 +0100

    Status bar expanded header: Customize icons and text, (1/2):
    - Option to show/hide the (default) battery status text next to the battery icon
    - Custom text color:
      - Time
      - Date (Transparency 70%, default for secondary text)
      - Battery level
      - Alarm status (Transparency 40%, default for the Alarm status text)
      - Weather
    - Custom icon color:
      - Settings button icon
      - Alarm status icon (Transparency 40%, default for the Alarm status icon)
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
    
    Change-Id: Ica32c6f6f7e0730ddfae7116b547795059a9ea80

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3388,6 +3388,19 @@ public final class Settings {
          */
         public static final String STATUS_BAR_BATTERY_TEXT_COLOR = "battery_status_text_color";
         
+
+		/**
+         * Color of the text on the header of the expanded status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_EXPANDED_HEADER_TEXT_COLOR = "status_bar_expanded_header_text_color";
+
+        /**
+         * Color of the icons on the header of the expanded status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_EXPANDED_HEADER_ICON_COLOR = "status_bar_expanded_header_icon_color";
+        
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
