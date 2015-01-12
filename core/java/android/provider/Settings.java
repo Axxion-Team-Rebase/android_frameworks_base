ac15226e5ab4a463eb55f9ee4d3b052bc2d120bb
maxwen <max.weninger@gmail.com>
2015-01-12 16:24:11 +0100

    FWB: omniswitch as default recents (squash) (1/2)
    AICPfy and bring to lollipop
    PS5: G2G
    
    Change-Id: I866806ee3601ac69eb877f10c10bb881a70b1013

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3492,6 +3492,12 @@ public final class Settings {
         public static final String STATUS_BAR_POWER_MENU = "status_bar_power_menu";
 
         /**
+         * Alternative recent apps integration using OmniSwitch
+         * @hide
+         */
+        public static final String RECENTS_USE_OMNISWITCH = "recents_use_omniswitch";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
