ae78efc89bf71987676c76988c0cbd01a2105893
Danesh M <daneshm90@gmail.com>
2015-01-18 00:33:00 -0800

    SystemUI: Lockscreen shortcut customization
    Change-Id: I4abf1deed0a342e111453516fa7820191a55ffc4
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
    
    Conflicts:
    	packages/SystemUI/res/values/cm_strings.xml
    
    Cleanup & optimization.
    
    Change-Id: I2815e814301788cfb1fbf68abb7de947b85c1669
    
    SystemUI : Add ability to disable lockscreen targets
    
    Change-Id: Iec684028a3056de47163058c44c0cfc252098fe1
    
    Conflicts:
    	packages/SystemUI/Android.mk
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -48,6 +48,9 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.internal.util.cm.LockscreenShortcutsHelper;
+import com.android.internal.widget.LockPatternUtils;
+
 import com.android.keyguard.KeyguardStatusView;
 import com.android.systemui.R;
 import com.android.systemui.qs.QSContainer;
@@ -1747,10 +1750,14 @@ public class NotificationPanelView extends PanelView implements
         boolean start = getLayoutDirection() == LAYOUT_DIRECTION_RTL ? rightPage : !rightPage;
         mIsLaunchTransitionRunning = true;
         mLaunchAnimationEndRunnable = null;
-        if (start) {
+        if (start) 
             mKeyguardBottomArea.launchPhone();
-        } else {
+        else if {
+            EventLogTags.writeSysuiLockscreenGesture(
+                    EventLogConstants.SYSUI_LOCKSCREEN_GESTURE_SWIPE_CAMERA, lengthDp, velocityDp);
             mSecureCameraLaunchManager.startSecureCameraLaunch();
+        } else { (!mKeyguardBottomArea.isTargetCustom(
+                LockscreenShortcutsHelper.Shortcuts.RIGHT_SHORTCUT))
         }
         mStatusBar.startLaunchTransitionTimeout();
         mBlockTouches = true;
@@ -1783,9 +1790,9 @@ public class NotificationPanelView extends PanelView implements
         });
         boolean start = getLayoutDirection() == LAYOUT_DIRECTION_RTL ? right : !right;
         if (start) {
-            mStatusBar.onPhoneHintStarted();
+            mStatusBar.onPhoneHintStarted(mKeyguardBottomArea.getLeftHint());
         } else {
-            mStatusBar.onCameraHintStarted();
+            mStatusBar.onCameraHintStarted(mKeyguardBottomArea.getRightHint());
         }
     }
 
