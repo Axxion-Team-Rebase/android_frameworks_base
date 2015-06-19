9116c689a9cc2f6a8e71a8560ad4b0f75d91fc3c
siracuervo <fl.siracusa@outlook.com.ar>
2015-06-19 14:39:27 -0300

    Base: Add option to perform soft-reboot.
    Using cm soft reboot method and adapted to yashade2001 logic
    
    Change-Id: I1a4e4bad922378192c929cf50ba92d6d72b7d968

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -28,6 +28,7 @@ import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.IActivityManager;
 import android.app.KeyguardManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -113,6 +114,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
     private static final String GLOBAL_ACTION_KEY_SETTINGS = "settings";
     private static final String GLOBAL_ACTION_KEY_LOCKDOWN = "lockdown";
     private static final String GLOBAL_ACTION_KEY_REBOOT = "reboot";
+    private static final String GLOBAL_ACTION_KEY_SOFT_REBOOT = "soft_reboot";
     private static final String GLOBAL_ACTION_KEY_REBOOT_RECOVERY = "reboot_recovery";
     private static final String GLOBAL_ACTION_KEY_REBOOT_BOOTLOADER = "reboot_bootloader";
 
@@ -362,10 +364,12 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             } else if (GLOBAL_ACTION_KEY_REBOOT.equals(actionKey)) {
                 // always enable the simple reboot
                 mItems.add(new RebootAction());
+			} else if (advancedRebootEnabled(mContext) && GLOBAL_ACTION_KEY_SOFT_REBOOT.equals(actionKey)) {
+                mItems.add(new SoftRebootAction());                   
             } else if (advancedRebootEnabled(mContext) && GLOBAL_ACTION_KEY_REBOOT_RECOVERY.equals(actionKey)) {
                 mItems.add(new RebootRecoveryAction());
             } else if (advancedRebootEnabled(mContext) && GLOBAL_ACTION_KEY_REBOOT_BOOTLOADER.equals(actionKey)) {
-                mItems.add(new RebootBootloaderAction());
+                mItems.add(new RebootBootloaderAction());             
             } else if (GLOBAL_ACTION_KEY_AIRPLANE.equals(actionKey)) {
 				if (mShowAirplaneToggle) {
                 mItems.add(mAirplaneModeOn);
@@ -551,6 +555,50 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         }
     }
 
+   private final class SoftRebootAction extends SinglePressAction {
+        private SoftRebootAction() {
+            super(com.android.internal.R.drawable.ic_lock_power_reboot,
+                    R.string.global_action_soft_reboot);
+        }
+
+        @Override
+        public boolean showDuringKeyguard() {
+            return true;
+        }
+
+        @Override
+        public boolean showDuringRestrictedKeyguard() {
+            return false;
+        }
+
+        @Override
+        public boolean showBeforeProvisioning() {
+            return true;
+        }
+
+        @Override
+        public boolean showForCurrentUser() {
+            return UserHandle.getCallingUserId() == UserHandle.USER_OWNER;
+        }
+
+        @Override
+        public void onPress() {
+			doSoftReboot();
+		}
+    }
+    
+   private static void doSoftReboot() {
+         try {
+             final IActivityManager am =
+                   ActivityManagerNative.asInterface(ServiceManager.checkService("activity"));
+             if (am != null) {
+                 am.restart();
+             }
+         } catch (RemoteException e) {
+             Log.e(TAG, "failure trying to perform soft reboot", e);
+         }
+     }
+    
     private Action getBugReportAction() {
         return new SinglePressAction(com.android.internal.R.drawable.ic_lock_bugreport,
                 R.string.bugreport_title) {
@@ -1455,7 +1503,12 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                     com.android.internal.R.array.config_rebootActionsList);
         for (int i = 0; i < rebootMenuActions.length; i++) {
             String actionKey = rebootMenuActions[i];
-            if (advancedRebootEnabled(mContext) && GLOBAL_ACTION_KEY_REBOOT_RECOVERY.equals(actionKey)) {
+			if (advancedRebootEnabled(mContext) && GLOBAL_ACTION_KEY_SOFT_REBOOT.equals(actionKey)) {
+                SoftRebootAction a = new SoftRebootAction();
+                if (isActionVisible(a)) {
+                    items.add(a);
+                }	
+			} else if (advancedRebootEnabled(mContext) && GLOBAL_ACTION_KEY_REBOOT_RECOVERY.equals(actionKey)) {
                 RebootRecoveryAction a = new RebootRecoveryAction();
                 if (isActionVisible(a)) {
                     items.add(a);
