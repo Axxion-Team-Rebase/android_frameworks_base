4c3f268e9b7a6c647f4233a1ef65d934abedbb6b
siracuervo <fl.siracusa@outlook.com.ar>
2015-06-30 16:18:53 -0300

    Pass floating to some other places.
    Taken from https://github.com/AICP/frameworks_base/commit/917729c19770e305337611e783d41be7bc2daa7b#diff-71d90e86a8e8a7f4c400cdaf7fd10cbf
    
    And also some code for phonewindow.
    
    I cannot add authorship here cause no one there made the code im adding here, only Carlo Savignano from PA for PhoneWindow
    
    Change-Id: I182db2c590b06eb81d8913ede580cf95fcfb47eb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3636,6 +3636,12 @@ public final class Settings {
         public static final String HEADS_UP_FLOATING = "heads_up_floating";
 
         /**
+         * Option to launch slim actions in floating mode
+         * @hide
+         */
+        public static final String SLIM_ACTION_FLOATS = "slim_action_floats";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
