41900c30184eeb1fbad809821f7ba53a6d828c0f
siracuervo <fl.siracusa@outlook.com.ar>
2015-07-03 22:45:00 -0300

    Utils: Add missed method to ImageHelper
    Change-Id: I994947df7756813a5edd21fe9369812a01cad927

--- core/java/com/android/internal/util/slim/ImageHelper.java
+++ core/java/com/android/internal/util/slim/ImageHelper.java
@@ -54,6 +54,18 @@ public class ImageHelper {
         cc.drawBitmap(grayscaleBitmap, rect, rect, pp);
         return grayscaleBitmap;
     }
+     
+    public static Bitmap drawableToBitmap (Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(), Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+		drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }    
 
     private static Bitmap toGrayscale(Bitmap bmpOriginal) {
         int width, height;
